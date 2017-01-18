414f5d66eb6a2498b358761767228e74af43c72f
Markus Hyvärinen <markus.hyvarinen@cs.tamk.fi>
2017-01-18 11:00:30 +0200

    Added UnitBase and IMover interface

---
+++ Assets/Code/Interfaces/IMover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace TAMKShooter
+{
+    public interface IMover
+    {
+        //Position of this object in world space
+        Vector3 position { get; set; }
+        //Rotation of this object in world space
+        Quaternion rotation { get; set; }
+        //The speed of this mover
+        float speed { get; }
+
+        //Moves toward target position
+        void MoveTowardPosition(Vector3 target);
+        //Moves to direction
+        void MoveToDirection(Vector3 direction);
+        //Rotates toward 'targetPosition'.
+        void RotateTowardPosition(Vector3 targetPosition);
+    }
+}
