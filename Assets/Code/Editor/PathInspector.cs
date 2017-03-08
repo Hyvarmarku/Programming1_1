d93a587a2c867c2a229b32db1e373401e89f911d
Markus Hyvärinen <markus.hyvarinen@cs.tamk.fi>
2017-03-08 13:54:17 +0200

    WaypointSystem/pathfind

---
+++ Assets/Code/Editor/PathInspector.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UE = UnityEditor;
+using TAMKShooter.WaypointSystem;
+
+namespace TAMKShooter.Editor
+{
+    [UE.CustomEditor(typeof(Path))]
+    public class PathInspector : UE.Editor
+    {
+        private const string ButtonText = "Add waypoint";
+        private Path _target;
+
+        private void OnEnable()
+        {
+            _target = target as Path;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button(ButtonText))
+            {
+                int waypointCount = _target.transform.childCount;
+                string waypointName = string.Format("Waypoint{0:D3}", (waypointCount + 1));
+                GameObject waypoint = new GameObject(waypointName,typeof(Waypoint));
+                waypoint.transform.SetParent(_target.transform);
+                UE.Selection.activeGameObject = waypoint;
+            }
+        }
+    }
+}
---
+++ Assets/Code/WaypointSystem/Path.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace TAMKShooter.WaypointSystem
+{
+
+    public class Path : MonoBehaviour
+    {
+        public List<Waypoint> Waypoints
+        {
+            get
+            {
+                if (_waypoints == null || _waypoints.Count == 0 || !Application.isPlaying)
+                {
+                    _waypoints = GetComponentsInChildren<Waypoint>().ToList();
+                }
+                return _waypoints;
+            }
+        }
+
+        [SerializeField]
+        private PathType _pathType;
+        private List<Waypoint> _waypoints = new List<Waypoint>();
+        private readonly Dictionary<PathType, Color> _pathColors =
+            new Dictionary<PathType, Color>()
+            {
+                {PathType.Loop, Color.green },
+                {PathType.PingPong, Color.red },
+                {PathType.OneWay, Color.blue }
+            };
+
+        public Waypoint GetClosestWaypoint(Vector3 position)
+        {
+            float smallestSqrMagnitude = float.PositiveInfinity;
+            Waypoint closests = null;
+
+            foreach (var waypoint in Waypoints)
+            {
+                Vector3 toWaypointVector = waypoint.Position - position;
+                float currentSqrMagnitude = toWaypointVector.sqrMagnitude;
+
+                if (currentSqrMagnitude < smallestSqrMagnitude)
+                {
+                    smallestSqrMagnitude = currentSqrMagnitude;
+                    closests = waypoint;
+                }
+            }
+            return closests;
+        }
+
+        public Waypoint GetNextWaypoint(Waypoint currentWaypoint, ref Direction direction)
+        {
+            Waypoint nextWaypoint = null;
+
+            for (int i = 0; i < Waypoints.Count; i++)
+            {
+                if (Waypoints[i] == currentWaypoint)
+                {
+                    switch (_pathType)
+                    {
+                        case PathType.Loop:
+                            nextWaypoint = GetNextWaypointLoop(i, direction);
+                            break;
+                        case PathType.OneWay:
+                            nextWaypoint = GetNextWaypointOneWay(i, direction);
+                            break;
+                        case PathType.PingPong:
+                            nextWaypoint = GetNextWaypointPingPong(i, ref direction);
+                            break;
+                    }
+                }
+            }
+
+            return nextWaypoint;
+        }
+
+        private Waypoint GetNextWaypointPingPong(int currentWaypointIndex, ref Direction direction)
+        {
+            Waypoint nextWaypoint = null;
+
+            switch (direction)
+            {
+                case Direction.Forward:
+                    if (currentWaypointIndex < Waypoints.Count - 1)
+                    {
+                        nextWaypoint = Waypoints[currentWaypointIndex + 1];
+                    }
+                    else
+                    {
+                        nextWaypoint = Waypoints[currentWaypointIndex - 1];
+                        direction = Direction.Backward;
+                    }
+                    break;
+                case Direction.Backward:
+                    if (currentWaypointIndex > 0)
+                    {
+                        nextWaypoint = Waypoints[currentWaypointIndex - 1];
+                    }
+                    else
+                    {
+                        nextWaypoint = Waypoints[currentWaypointIndex + 1];
+                        direction = Direction.Forward;
+                    }
+                    break;
+            }
+
+            return nextWaypoint;
+        }
+
+        private Waypoint GetNextWaypointOneWay(int currentWaypointIndex, Direction direction)
+        {
+            Waypoint nextWaypoint = null;
+
+            switch (direction)
+            {
+                case Direction.Forward:
+                    if (currentWaypointIndex < Waypoints.Count - 1)
+                    {
+                        nextWaypoint = Waypoints[currentWaypointIndex + 1];
+                    }
+                    break;
+                case Direction.Backward:
+                    if (currentWaypointIndex > 0)
+                    {
+                        nextWaypoint = Waypoints[currentWaypointIndex - 1];
+                    }
+                    break;
+            }
+
+            return nextWaypoint;
+        }
+
+        private Waypoint GetNextWaypointLoop(int currentWaypointIndex, Direction direction)
+        {
+            //Waypoint nextWaypoint = null;
+
+            //switch (direction)
+            //{
+            //    case Direction.Forward:
+            //        if (currentWaypointIndex < Waypoints.Count - 1)
+            //        {
+            //            nextWaypoint = Waypoints[currentWaypointIndex + 1];
+            //        }
+            //        else
+            //        {
+            //            nextWaypoint = Waypoints[0];
+            //        }
+            //        break;
+            //    case Direction.Backward:
+            //        if (currentWaypointIndex > 0)
+            //        {
+            //            nextWaypoint = Waypoints[currentWaypointIndex - 1];
+            //        }
+            //        else
+            //        {
+            //            nextWaypoint = Waypoints[Waypoints.Count -1];
+            //        }
+            //        break;
+            //}
+           //return nextWaypoint;
+           return direction == Direction.Forward 
+                ? Waypoints[++currentWaypointIndex % Waypoints.Count]
+                : Waypoints[(--currentWaypointIndex >=0
+                ? currentWaypointIndex
+                : Waypoints.Count -1) % Waypoints.Count];
+                                                                                            
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = _pathColors[_pathType];
+            if (Waypoints.Count > 1)
+            {
+                for (int i = 1; i < Waypoints.Count; ++i)
+                {
+                    Gizmos.DrawLine(Waypoints[i -1].Position, Waypoints[i].Position);
+                }
+                if (_pathType == PathType.Loop)
+                {
+                    Gizmos.DrawLine(Waypoints[_waypoints.Count - 1].Position, Waypoints[0].Position);
+                }
+            }
+        }
+    }
+}
